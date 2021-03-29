7c3db247dff7ffab867dd7c5782e80fe1b688306
Diego Auyón <diegoeauyonh@gmail.com>
2021-03-29 10:34:43 -0600

    Update chart styles

--- chart.js
+++ chart.js
@@ -1,13 +1,16 @@
 import Chart from 'chart.js/dist/Chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-Chart.helpers.merge(Chart.defaults.global.plugins.datalabels, {
-    align: 'start',
-    anchor: 'end',
-    offset: -20,
-    display: true,
-    font: {
-        weight: 'bold'
-    },
-    color: '#666666'
-});
+Chart.plugins.register(ChartDataLabels);
+
+// Chart.helpers.merge(Chart.defaults.global.plugins.datalabels, {
+//     align: 'start',
+//     anchor: 'end',
+//     offset: -20,
+//     display: true,
+//     font: {
+//         weight: 'bold'
+//     },
+//     color: '#666666'
+// });
+export default Chart;
